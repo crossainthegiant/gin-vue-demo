63c0af53ba73a8c8133bcc0fcf3c1e3c52ab58d0
crossain Qi <crossain@outlook.com>
2021-11-15 15:55:52 +0800

    添加登录功能

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,6 +5,7 @@ import (
 	"github.com/crossainthegiant/gin-vue/model"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -48,11 +49,16 @@ func Register(c *gin.Context) {
 		return
 	}
 	//创建用户
-	newUser := model.User{
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"code": 502, "msg": "加密错误"})
+		return
+	}
 
+	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hashedPassword),
 	}
 	DB.Create(&newUser)
 	//返回结果
@@ -68,4 +74,46 @@ func isTelExist(db *gorm.DB, telephone string) bool {
 		return true
 	}
 	return false
+}
+
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+	//数据验证
+	if len(telephone) != 11 {
+		c.JSON(422, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(422, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于六位",
+		})
+		return
+	}
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码错误"})
+		return
+	}
+	//发放Token
+	token := "111"
+	//返回结果
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登陆成功",
+	})
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 )
